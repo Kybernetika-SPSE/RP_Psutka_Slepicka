b179f8d5fe3d0b6f68a9831963670c472af68008
FrteenCZ <franta11franta@gmail.com>
2025-02-16 22:07:42 +0100

    added scan wifi function into a utils

---
+++ src/main/src/utils/wifi.cpp
@@ -0,0 +1,58 @@
+#include "wifi.h"
+
+//scan wifi a save it to scanned_networks vector
+void scan_wifi() {
+    //clear scanned_networks to ensure no outdated network entries remains
+    scanned_networks.clear();
+
+    Serial.println("scanning for networks ...");
+
+    //scan
+    int n = WiFi.scanNetworks();
+
+    //check if any networks found
+    if (n == 0) {
+        Serial.println("no networks found");
+        return;
+    } else {
+        //loop through all the networks
+        for (int i = 0; i < n; ++i) {
+            // Print BSSID, RSSI and SSID for each network found
+            Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x",  WiFi.BSSID(i)[0], WiFi.BSSID(i)[1], WiFi.BSSID(i)[2], 
+                                                            WiFi.BSSID(i)[3], WiFi.BSSID(i)[4], WiFi.BSSID(i)[5]);
+            Serial.print(" - ");
+            Serial.print(-WiFi.RSSI(i));
+            Serial.print(" - ");
+            Serial.print(WiFi.SSID(i));
+            Serial.print("\n");
+
+            //saving networks to vector
+            //first one
+            if (0 == scanned_networks.size()) {
+                scanned_networks.push_back({
+                        WiFi.SSID(i).c_str(),                   //SSID
+                        std::to_string(-WiFi.RSSI(i)),          //RSSI
+                        std::to_string(WiFi.encryptionType(i))  //AUTH
+                });
+
+            } else {
+                //loop through already saved networks
+                for (size_t j = 0; j < scanned_networks.size(); j++) {
+                    //check if the network is already saved
+                    if (std::string(WiFi.SSID(i).c_str()) == scanned_networks[j][0]){
+                        break;
+                    }
+                    
+                    //when no saved networks match, save it
+                    if (j == scanned_networks.size() - 1) {
+                        scanned_networks.push_back({
+                                WiFi.SSID(i).c_str(),                   //SSID
+                                std::to_string(-WiFi.RSSI(i)),          //RSSI
+                                std::to_string(WiFi.encryptionType(i))  //AUTH
+                        });
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/main/src/utils/wifi.h
@@ -0,0 +1,11 @@
+#ifndef UTIL_WIFI_H
+#define UTIL_WIFI_H
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <vector>
+
+
+extern std::vector<std::vector<std::string>> scanned_networks;
+
+#endif
