5a6805a6ad992a5e9808b9291c3f0bed86918058
FrteenCZ <franta11franta@gmail.com>
2025-03-23 21:19:15 +0100

    refactor: remove debug print statements from solveLeastSquares function

--- src/UWB_tracing_logic/src/leastSquare.cpp
+++ src/UWB_tracing_logic/src/leastSquare.cpp
@@ -2,14 +2,6 @@
 
 Matrix solveLeastSquares(const Matrix &A, const Matrix &b)
 {
-    Serial.println("Step 1: Printing matrix A:");
-    A.print();
-
-    Serial.println("Step 2: Printing vector b:");
-    b.print();
-
-    Serial.println("====================QR==========================");
-
     int rows = A.matrix.size();
     int cols = A.matrix[0].size();
 
@@ -19,22 +11,9 @@ Matrix solveLeastSquares(const Matrix &A, const Matrix &b)
     Q = qr_result.first;
     R = qr_result.second;
 
-    Serial.println("====================QR==========================");
-
-    Serial.println("Step 3: Q matrix:");
-    Q.print();
-
-    Serial.println("Step 4: R matrix:");
-    R.print();
-
     Matrix Qt = Q.transpose();
 
-    Serial.println("Step 5: Q^T:");
-    Qt.print();
-
     Matrix Qtb = Qt * b;
-    Serial.println("Step 6: Q^T * b:");
-    Qtb.print();
 
     Matrix x(Qtb.matrix.size(), 1);
 
@@ -55,7 +34,5 @@ Matrix solveLeastSquares(const Matrix &A, const Matrix &b)
         x[i][0] = (Qtb.matrix[i][0] - sum) / R.matrix[i][i];
     }
 
-    Serial.println("Step 7: Solution x:");
-    x.print();
     return x;
 }
