7db7e673d8ff6b4da87b78fc4b0cd2fc5f16ea88
FrteenCZ <franta11franta@gmail.com>
2025-03-26 22:06:50 +0100

    refactor: simplify solveLeastSquares function by using inverse of R for solution

--- src/UWB_tracing_logic/src/leastSquare.cpp
+++ src/UWB_tracing_logic/src/leastSquare.cpp
@@ -9,37 +9,14 @@
  */
 Matrix solveLeastSquares(const Matrix &A, const Matrix &b)
 {
-    int rows = A.matrix.size();
-    int cols = A.matrix[0].size();
-
-    // Perform QR decomposition
-    std::pair<Matrix, Matrix> qrResult = A.qrDecomposition();
-    Matrix Q = qrResult.first;
-    Matrix R = qrResult.second;
-
-    // Compute Q^T * b
-    Matrix Qt = Q.transpose();
-    Matrix Qtb = Qt * b;
-
-    // Initialize the solution vector x
-    Matrix x(cols, 1);
-
-    // Back-substitution to solve R * x = Q^T * b
-    for (int i = cols - 1; i >= 0; --i)
-    {
-        if (R.matrix[i][i] == 0.0)
-        {
-            Serial.println("Error: Singular matrix in upper triangular solve.");
-            return Matrix(0, 0);
-        }
-
-        float sum = 0.0;
-        for (int j = i + 1; j < cols; ++j)
-        {
-            sum += R.matrix[i][j] * x[j][0];
-        }
-        x[i][0] = (Qtb.matrix[i][0] - sum) / R.matrix[i][i];
-    }
-
-    return x;
+       // Perform QR decomposition
+       std::pair<Matrix, Matrix> qrResult = A.qrDecomposition();
+       Matrix Q = qrResult.first;
+       Matrix R = qrResult.second;
+   
+       // Compute Q^T * b
+       Matrix Qtb = Q.transpose() * b;
+   
+       // Solve for x using inverse of R
+       return R.inverseQR() * Qtb;
 }
