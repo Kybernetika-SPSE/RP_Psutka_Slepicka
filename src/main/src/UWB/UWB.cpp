84e5dee04512519daac60981ec6ff37adf122365
FrteenCZ <franta11franta@gmail.com>
2025-04-17 17:20:42 +0200

    feat: UWB finally works, meassures distances, runtime mode switch

--- src/main/src/UWB/UWB.cpp
+++ src/main/src/UWB/UWB.cpp
@@ -1,56 +1,10 @@
 #include "UWB.h"
 
-#ifndef TAG_MODE
-#define ANCHOR_ADD "83:17:5B:D5:A9:9A:E2:9C"
+#ifdef ANCHORMODE
+bool isAnchor = true;
 #else
-#define TAG_ADD "7D:00:22:EA:82:60:3B:9C"
+bool isAnchor = false;
 #endif
-
-void newRange();
-void newBlink(DW1000Device *device);
-void inactiveDevice(DW1000Device *device);
-void newDevice(DW1000Device *device);
-
-void UWB_setup()
-{
-    Serial.begin(115200);
-    delay(1000);
-
-    SPI.begin(UWB_PIN_SPI_SCK, UWB_PIN_SPI_MISO, UWB_PIN_SPI_MOSI);
-
-    DW1000Ranging.initCommunication(UWB_PIN_SPI_RST, UWB_PIN_SPI_SS, UWB_PIN_SPI_IRQ);
-
-    DW1000Ranging.attachNewRange(newRange);
-#ifdef TAG_MODE
-    DW1000Ranging.attachNewDevice(newDevice);
-#else
-    DW1000Ranging.attachBlinkDevice(newBlink);
-#endif
-
-    DW1000Ranging.attachInactiveDevice(inactiveDevice);
-
-    // Enable the filter to smooth the distance
-    // DW1000Ranging.useRangeFilter(true);
-    DW1000.setChannel(5); // Use the same channel for both tag and anchor
-
-#ifdef TAG_MODE
-    // we start the module as a tag
-    DW1000Ranging.startAsTag(TAG_ADD, DW1000.MODE_LONGDATA_FAST_ACCURACY);
-#else
-    // we start the module as an anchor
-    //  DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_RANGE_LOWPOWER, false);
-    //  DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_SHORTDATA_FAST_LOWPOWER);
-    //  DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_FAST_LOWPOWER);
-    //  DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_SHORTDATA_FAST_ACCURACY);
-    //  DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_FAST_ACCURACY);
-    DW1000Ranging.startAsAnchor(ANCHOR_ADD, DW1000.MODE_LONGDATA_FAST_ACCURACY, false);
-#endif
-}
-
-void UWB_loop()
-{
-    DW1000Ranging.loop();
-}
 
 void newRange()
 {
@@ -64,16 +18,15 @@ void newRange()
     Serial.println(" dBm");
 }
 
-void newBlink(DW1000Device *device)
+void newDevice(DW1000Device *device)
 {
-    Serial.print("blink; 1 device added ! -> ");
-    Serial.print(" short:");
+    Serial.print("New device added -> Short: ");
     Serial.println(device->getShortAddress(), HEX);
 }
 
-void newDevice(DW1000Device *device)
+void newBlink(DW1000Device *device)
 {
-    Serial.print("ranging init; 1 device added ! -> ");
+    Serial.print("blink; 1 device added ! -> ");
     Serial.print(" short:");
     Serial.println(device->getShortAddress(), HEX);
 }
@@ -82,4 +35,68 @@ void inactiveDevice(DW1000Device *device)
 {
     Serial.print("delete inactive device: ");
     Serial.println(device->getShortAddress(), HEX);
+}
+
+void startAsTag()
+{
+    Serial.println("Starting as TAG...");
+    DW1000Ranging.attachNewRange(newRange);
+    DW1000Ranging.attachNewDevice(newDevice);
+    DW1000Ranging.attachInactiveDevice(inactiveDevice);
+    DW1000Ranging.startAsTag("7D:00:22:EA:82:60:3B:9C", DW1000.MODE_LONGDATA_RANGE_ACCURACY);
+}
+
+void startAsAnchor()
+{
+    Serial.println("Starting as ANCHOR...");
+    DW1000Ranging.attachNewRange(newRange);
+    DW1000Ranging.attachBlinkDevice(newBlink);
+    DW1000Ranging.attachInactiveDevice(inactiveDevice);
+    DW1000Ranging.startAsAnchor("82:17:5B:D5:A9:9A:E2:9C", DW1000.MODE_LONGDATA_RANGE_ACCURACY);
+}
+
+
+void UWB_setup()
+{
+    Serial.begin(115200);
+    delay(1000);
+    SPI.begin(14, 12, 13); // SCK, MISO, MOSI
+    // init the configuration
+    DW1000Ranging.initCommunication(UWB_PIN_SPI_RST, UWB_PIN_SPI_SS, UWB_PIN_SPI_IRQ); // Reset, CS, IRQ pin
+
+    if (isAnchor)
+    {
+        startAsAnchor();
+    }
+    else
+    {
+        startAsTag();
+    }
+}
+
+void UWB_loop()
+{
+    DW1000Ranging.loop();
+}
+
+void switchMode()
+{
+    Serial.println("Switching mode...");
+
+    // This is a workaround to "stop" current ranging
+    detachInterrupt(digitalPinToInterrupt(UWB_PIN_SPI_IRQ));
+
+    // Re-init SPI communication
+    DW1000Ranging.initCommunication(UWB_PIN_SPI_RST, UWB_PIN_SPI_SS, UWB_PIN_SPI_IRQ);
+
+    if (isAnchor)
+    {
+        isAnchor = false;
+        startAsTag();
+    }
+    else
+    {
+        isAnchor = true;
+        startAsAnchor();
+    }
 }
--- src/main/src/UWB/UWB.h
+++ src/main/src/UWB/UWB.h
@@ -1,17 +1,17 @@
 #ifndef UWB_H
 #define UWB_H
 
-//#include <DW1000.h>
-#include <DW1000Ranging.h>
-#include <WebServer.h>
-#include <ArduinoJson.h>
 #include <SPI.h>
-#include "esp_task_wdt.h"
-
+#include "DW1000Ranging.h"
+#include <WebServer.h>
 
 #include "config.h"
 
 extern WebServer server;
+
+
+extern bool isAnchor; // true if the device is an anchor, false if it is a tag
+
 
 void UWB_setup();
 void UWB_loop();
